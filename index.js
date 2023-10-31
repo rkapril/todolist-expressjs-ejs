96eb24d8f631eb5a69e2571a14d5ab0d6f1d1fc0
Ray Goh <61505106+rkapril@users.noreply.github.com>
2023-10-31 18:11:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const taskList = [];
 
 app.get("/", (req, res) => {
-  res.render("index.ejs", { taskList}); 
+  res.render("index.ejs", { taskList });
 });
 
 app.post("/add", (req, res) => {
